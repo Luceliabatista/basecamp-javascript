a4a629c6e18ebe0d71069eaf2269f6cf7cb3e8ab
Lucélia Batista <lucelia.ti@gmail.com>
2022-06-04 14:58:25 -0300

    validação incluída

--- sintaxe-e-operadores/comparacaoNumeros.js
+++ sintaxe-e-operadores/comparacaoNumeros.js
@@ -1,4 +1,6 @@
 function verificacaoPrincipal(num1, num2) {
+    if (!num1 || !num2) return ('É necessário definir 2 números diferentes de 0')
+
     const fase1 = comparacao1(num1, num2)
     const fase2 = comparacao2(num1, num2)
 
@@ -6,9 +8,9 @@ function verificacaoPrincipal(num1, num2) {
 }
 
 function comparacao1(num1, num2) {
-    if (num1 == num2) {
+    if (num1 === num2) {
         return (`Os números ${num1} e ${num2} são iguais.`)
-    } else if (num1 != num2) {
+    } else if (num1 !== num2) {
         return (`Os números ${num1} e ${num2} não são iguais.`)
     }
 }
@@ -29,13 +31,13 @@ function comparacao2(num1, num2) {
     if (soma < parametro1) {
         compara10 = 'menor'
     }
-    if (soma == parametro1) {
+    if (soma === parametro1) {
         compara10 = 'igual'
-    } 
-    if (soma == parametro2) {
+    }
+    if (soma === parametro2) {
         compara20 = 'igual'
     }
-    return (`Sua soma é ${soma}, que é ${compara10} que 10 e ${compara20} que 20`)
+    return (`Sua soma é ${soma}, que é ${compara10} que 10 e ${compara20} que 20.`)
 }
 
-console.log(verificacaoPrincipal(10, 10)) 
+console.log(verificacaoPrincipal(2, 0)) 
